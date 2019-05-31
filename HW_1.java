f9af52576ab1c217e1dfa0caef57735910131c97
wiizzaard <49917242+wiizzaard@users.noreply.github.com>
2019-05-31 16:28:54 +0300

    Add files via upload

---
+++ HW_1.java
@@ -0,0 +1,11 @@
+public class HW_1 {
+    public static void main(String [] arg)
+
+    {
+        String name = "Вася";
+        int height = 168;
+        float mass = 60, 5;
+        System.out.println("Имя = ") name;
+
+    }
+}
