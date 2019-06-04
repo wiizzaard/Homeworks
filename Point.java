c9a06807305bc0e20b54fa30ac2ff8021964875c
wiizzaard <49917242+wiizzaard@users.noreply.github.com>
2019-06-04 19:21:36 +0300

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,17 @@
+public class Point {
+    int x;
+    int y;
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+        }
+
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void info() {
+        System.out.println();
+    }
+}
